3a5814090470d32e6e532817c707886afb6bcbb5
violapoma <violapoma@gmail.com>
2025-04-24 17:19:59 +0200

    aggiunto dei file js

---
+++ Build Week/welcome.js
@@ -0,0 +1,23 @@
+let proceedBtn = document.getElementById("proceed");
+window.onload
+//document.getElementById("proceed").setAttribute("disabled", "true");
+
+let controlCheckBox = document.getElementById("toCheck");
+
+//event listener per il checkbox, se è flaggato, button abilitato
+controlCheckBox.addEventListener("change", enableProceedButton);
+
+//Abilita il button proceed solo se il checkbox è selezionato
+function enableProceedButton() {
+    if (controlCheckBox.checked) {
+        proceedBtn.removeAttribute("disabled");
+    } else {
+        proceedBtn.setAttribute("disabled", "true");
+    }
+}
+
+proceedBtn.addEventListener("click", showNextPage); 
+
+function showNextPage() {
+    window.location.href = "questions2.html";
+}
