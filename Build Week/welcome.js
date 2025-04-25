b04ee8c2c6d65521a5247410dac8271fa29e9083
violapoma <violapoma@gmail.com>
2025-04-25 20:42:13 +0200

    popup per spunta
    + welcome.html: il popup è in realtà uno span che appare al bisogno
    + welcome.css: ho aggiunto le classi per gestire il popup
    + welcome.js: ho riorganizzato un po' il codice (da migliorare) e aggiunto le funzioni per il popup
    + paginaEsame.css: ho giusto messo il cursor pointer sulle domande

--- Build Week/welcome.js
+++ Build Week/welcome.js
@@ -1,36 +1,41 @@
 let proceedBtn = document.getElementById("proceed");
 document.getElementById("proceed").setAttribute("disabled", "true");
 
+//event listener per mettere di default il button disabled
+window.addEventListener("load",enableProceedButton);
+
 let controlCheckBox = document.getElementById("toCheck");
+//event listener per il checkbox, se è flaggato, button abilitato e popup non visibile
 
-
-//event listener per il checkbox, se è flaggato, button abilitato
+controlCheckBox.addEventListener("change", alreadyFlagged);
 controlCheckBox.addEventListener("change", enableProceedButton);
+proceedBtn.addEventListener("click", showNextPage);
+//messaggio di aiuto all'utente per procedere con l'esame
+
+
 
 //Abilita il button proceed solo se il checkbox è selezionato
 function enableProceedButton() {
     if (controlCheckBox.checked) {
+        proceedBtn.classList.remove("disabled");
         proceedBtn.removeAttribute("disabled");
+        document.getElementById("proceed").style.cursor = "pointer";
     } else {
+        proceedBtn.classList.add("disabled");
         proceedBtn.setAttribute("disabled", "true");
+        document.getElementById("proceed").style.cursor = "not-allowed";
+
     }  
 }
-
-proceedBtn.addEventListener("click", showNextPage);
-
-//messaggio di aiuto all'utente per procedere con l'esame
-proceedBtn.addEventListener("mouseover", obbligoFlag);
+//Mostra il popup se la checkbox non è selezionata
+function alreadyFlagged() {
+    if (controlCheckBox.checked) {
+        document.getElementById("popupText").style.display = "none";
+    } else {
+        document.getElementById("popupText").style.display = "flex";
+    }
+}
 
 function showNextPage() {
     window.location.href = "Pagina-esame.html";
 }
-
-//funzione per far apparire un messaggino di errore
-function obbligoFlag() {
-    if (controlCheckBox.checked) 
-        proceedBtn.removeAttribute("title");
-    
-    else
-        proceedBtn.setAttribute("title", "Devi confermare che eseguirai l'esame senza aiuti esterni!");
-    
-}   
